76ce85967c2856195ff9f3cb5e670221c76b550e
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-02-25 19:19:46 +0530

    Update testdocsmenu.js

--- src/components/testdocsmenu.js
+++ src/components/testdocsmenu.js
@@ -15,30 +15,7 @@ export default class DocsMenu extends React.Component {
               <div id="docs-nav-inside" className="docs-nav-inside sticky">
                 <button id="docs-nav-toggle" className="docs-nav-toggle">Navigate Docs<span className="icon-angle-right" aria-hidden="true" /></button>
                 <div className="docs-toc-wrap">
-                  <ul id="docs-toc" className="docs-toc">
-                    <li className={classNames('toc-item', {'current': _.get(page, 'relativePath') === _.get(root_page, 'relativePath')})}>
-                      <Link to={safePrefix(_.get(root_page, 'url'))}>{_.get(root_page, 'frontmatter.title')}</Link>
-                    </li>
-                    {_.map(_.get(site, 'data.doc_sections.sections'), (section, section_idx) => {
-                        let section_folder = _.get(site, 'data.doc_sections.root_folder') + section;
-                        let section_page_path = section_folder + '/index.md';
-                        let section_page = getPage(this.props.pageContext.pages, section_page_path);
-                        let child_pages = _.orderBy(_.filter(getPages(this.props.pageContext.pages, section_folder), item => _.get(item, 'base') != 'index.md'), 'frontmatter.weight');
-                        let child_count = _.size(child_pages);
-                        let has_children = (child_count > 0) ? true : false;
-                        let is_current_page = (_.get(page, 'relativePath') === _.get(section_page, 'relativePath')) ? true : false;
-                        let is_active = ((_.get(page, 'relativeDir') === _.get(section_page, 'relativeDir')) || (_.get(page, 'relativePath') === _.get(section_page, 'relativePath'))) ? true : false;
-                        return (<React.Fragment key={section_idx}>
-                          <li key={section_idx} className={classNames('toc-item', {'has-children': has_children, 'current': is_current_page, 'active': is_active})}>
-                            <Link to={safePrefix(_.get(section_page, 'url'))}>{_.get(section_page, 'frontmatter.title')}</Link>
-                            {has_children && <React.Fragment>
-                              <button className="submenu-toggle"><span className="screen-reader-text">Submenu</span><span className="icon-angle-right" aria-hidden="true" /></button>
-                              <DocsSubmenu {...this.props} child_pages={child_pages} page={page} site={site} />
-                            </React.Fragment>}
-                          </li>
-                        </React.Fragment>)
-                    })}
-                  </ul>
+                  <ul id="docs-toc" class="docs-toc"><li class="toc-item current"><a aria-current="page" class="" href="/docs/">Welcome to Libris</a></li><li class="toc-item has-children"><a href="/docs/about/">About</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/about/overview/">Overview</a></li><li class="toc-item"><a href="/docs/about/features/">Features</a></li></ul></li><li class="toc-item has-children"><a href="/docs/getting-started/">Getting Started</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/getting-started/installation/">Installation</a></li><li class="toc-item"><a href="/docs/getting-started/quick-start/">Quick Start</a></li></ul></li><li class="toc-item has-children"><a href="/docs/ui-components/">UI Components</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/ui-components/typography/">Typography</a></li></ul></li><li class="toc-item"><a href="/docs/manage-content/">Manage Content</a></li><li class="toc-item has-children"><a href="/docs/tools/">Tools</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/tools/starter-theme/">Starter Theme</a></li><li class="toc-item"><a href="/docs/tools/plug-ins/">Plug-ins</a></li></ul></li><li class="toc-item"><a href="/docs/faq/">FAQ</a></li><li class="toc-item"><a href="/docs/community/">Where To Get Support</a></li></ul>
                 </div>
               </div>
             </nav>
